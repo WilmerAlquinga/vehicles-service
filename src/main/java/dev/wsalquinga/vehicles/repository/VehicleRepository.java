6e1c149b41a0fd669214a4ec54bb8520f7883eec
wsalquinga <wilmer_alquinga@outlook.es>
2023-11-30 09:24:34 -0500

    fix: use Param annotation in repository / remove unused method

--- src/main/java/dev/wsalquinga/vehicles/repository/VehicleRepository.java
+++ src/main/java/dev/wsalquinga/vehicles/repository/VehicleRepository.java
@@ -5,11 +5,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 
 /**
  * @author wsalquinga on 31/10/2023
@@ -18,18 +18,12 @@ import java.util.Optional;
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
     @Query("SELECT v FROM Vehicle v " +
             "where (v.deletedAt is null " +
-            "or v.deletedAt > current_timestamp)")
+            "or v.deletedAt < current_timestamp)")
     Page<Vehicle> findAllValid(Pageable pageable);
 
     @Query("SELECT v FROM Vehicle v " +
             "where (v.deletedAt is null " +
-            "or v.deletedAt > current_timestamp) " +
-            "and v.id = :id")
-    Optional<Vehicle> findValidById(Long id);
-
-    @Query("SELECT v FROM Vehicle v " +
-            "where (v.deletedAt is null " +
-            "or v.deletedAt > current_timestamp) " +
+            "or v.deletedAt < current_timestamp) " +
             "and v.purchaseDate between :fromPeriod and :endPeriod")
-    List<Vehicle> findAllByPeriod(LocalDate fromPeriod, LocalDate endPeriod);
+    List<Vehicle> findAllByPeriod(@Param("fromPeriod") LocalDate fromPeriod, @Param("endPeriod") LocalDate endPeriod);
 }
