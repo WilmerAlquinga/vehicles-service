e3613be7a0ceb150b28262c96a5ba24e1a70cbed
wsalquinga <wilmer_alquinga@outlook.es>
2023-11-30 19:23:21 -0500

    feat: enable saving audit fields

---
+++ src/main/java/dev/wsalquinga/vehicles/config/AuditConfig.java
@@ -0,0 +1,12 @@
+package dev.wsalquinga.vehicles.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+/**
+ * @author wsalquinga on 30/11/2023
+ */
+@Configuration
+@EnableJpaAuditing
+public class AuditConfig {
+}
